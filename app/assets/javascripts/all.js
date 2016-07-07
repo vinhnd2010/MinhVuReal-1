ac22223a7cf2b2637357a138f7168d02029c3742
trungnn142 <trungnn142@gmail.com>
2016-07-07 22:52:40 +0700

    ref #N/A custom ui layout

---
+++ app/assets/javascripts/all.js
@@ -0,0 +1,71 @@
+(function($){
+  "use strict"; // Start of use strict
+
+  /* ---------------------------------------------
+    Scripts initialization
+  --------------------------------------------- */
+  $(window).load(function(){
+    $(window).trigger("scroll");
+    $(window).trigger("resize");
+  });
+
+  $(document).ready(function(){
+    $(window).trigger("resize");
+
+    $("a#mobile-menu").click(function(){
+      $(".mobile-menu").slideToggle("slow");
+    });
+  });
+
+  $(window).resize(function(){
+    init_classic_menu_resize();
+  });
+
+  /* --------------------------------------------
+   Platform detect
+   --------------------------------------------- */
+  var mobileTest;
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    mobileTest = true;
+    $("html").addClass("mobile");
+  }
+  else {
+    mobileTest = false;
+    $("html").addClass("no-mobile");
+  }
+
+  var mozillaTest;
+  if (/mozilla/.test(navigator.userAgent)) {
+    mozillaTest = true;
+  }
+  else {
+    mozillaTest = false;
+  }
+  var safariTest;
+  if (/safari/.test(navigator.userAgent)) {
+    safariTest = true;
+  }
+  else {
+    safariTest = false;
+  }
+
+  // Detect touch devices
+  if (!("ontouchstart" in document.documentElement)) {
+    document.documentElement.className += " no-touch";
+  }
+
+  /* ---------------------------------------------
+    Menu classic
+  --------------------------------------------- */
+  function init_classic_menu_resize(){
+    if ($(window).width() <= 1024) {
+      $(".wrapper").addClass("mobile-on");
+    }
+    else
+      if ($(window).width() > 1024) {
+        $(".wrapper").removeClass("mobile-on");
+        $(".mobile-menu").hide();
+        // desktop_nav.show();
+      }
+  }
+})(jQuery);
